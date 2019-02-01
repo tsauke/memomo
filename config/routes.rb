0f8d11e74dc4b57b044dc0a8a74d7a15d52187fb
tsauke <tasukesuke@gmail.com>
2019-02-02 01:30:51 +0900

    add show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             controllers: { registrations: 'registrations' }
   root 'pages#home'
+  resources :users, only: %i(show)
 end
---
+++ db/migrate/20190201160546_add_profile_photo_column_to_users.rb
@@ -0,0 +1,5 @@
+class AddProfilePhotoColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :profile_photo, :string
+  end
+end
