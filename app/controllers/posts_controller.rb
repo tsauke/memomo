d5398ef0ad6e470879271f3826a855c717c9f6ac
tsauke <tasukesuke@gmail.com>
2019-02-02 19:20:07 +0900

    iine

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i(show destroy)
 
   def index
-    @posts = Post.limit(10).includes(:photos, :user).order('created_at DESC')
+    @posts = Post.limit(10).includes(:photos, :user, :likes).order('created_at DESC')
   end
 
   def new
@@ -28,6 +28,7 @@ class PostsController < ApplicationController
 
   def show
     @photos = @post.photos
+    @likes = @post.likes.includes(:user)
   end
 
   def destroy
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
+  has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
+
+  def liked_by(user)
+    # user_idとpost_idが一致するlikeを検索する
+    Like.find_by(user_id: user.id, post_id: id)
+  end
 end
