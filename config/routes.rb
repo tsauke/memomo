0ce3e14809d6516c7e2d94ec786ca1cdc7344205
tsauke <tasukesuke@gmail.com>
2019-02-02 19:02:40 +0900

    add post show delete

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i(show)
 
-  resources :posts, only: %i(index new create) do
+  resources :posts, only: %i(index new create show destroy) do
     resources :photos, only: %i(create)
   end
 
